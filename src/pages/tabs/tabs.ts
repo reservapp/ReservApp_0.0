f3d9738992d04684639227d97a0077101f203064
Josias Ramon Carmona Amparo <joracaam@gmail.com>
2019-02-08 18:13:38 -0400

    Push_2 (Cuentas)

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-import { AccountPage } from '../account/account';
+//import { AccountPage } from '../account/account';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +13,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
-  tab4Root = AccountPage;
+  tab4Root = "AccountPage";
 
   constructor() {
 
