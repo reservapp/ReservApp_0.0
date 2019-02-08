078c91fc996a7cdfb7eae931ed664b4c87e9266f
Josias Ramon Carmona Amparo <joracaam@gmail.com>
2019-02-08 18:30:30 -0400

    Push_2(Cuentas)
    Le quite el import que tenia en el compenente tabs en el tabs.ts que pertenecia al AccountPage

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-//import { AccountPage } from '../account/account';
 
 @Component({
   templateUrl: 'tabs.html'
