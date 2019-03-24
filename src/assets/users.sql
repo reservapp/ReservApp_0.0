5d85c849d37b61bf18258bc7e7d25fd7d591bb43
Josias Ramon Carmona Amparo <joracaam@gmail.com>
2019-03-24 12:49:32 -0400

    Cambio base de datos

--- src/assets/users.sql
+++ src/assets/users.sql
@@ -1,4 +1,25 @@
-CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, userLastname TEXT, email TEXT, userPassword TEXT, phone TEXT);
+CREATE TABLE IF NOT EXISTS users(
+    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userName TEXT,
+    userLastname TEXT,
+    email TEXT,
+    userPassword TEXT,
+    phone TEXT);
+
+CREATE TABLE IF NOT EXISTS paymentMethod(
+    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId TEXT,
+    cardNumber TEXT,
+    expiration TEXT,
+    cvv TEXT,
+    billingAddress TEXT,
+    FOREIGN KEY(userId) REFERENCES users(id),);
+
 INSERT INTO users (userName, userLastname, email, userPassword, phone) VALUES ('Josias', 'Carmona', 'josca@mail.com', 'p@ssword', '8091234567');
 INSERT INTO users (userName, userLastname, email, userPassword, phone) VALUES ('Jhonncell', 'Jimenez', 'jhoji@mail.com', 'p@ssword', '8091234567');
 INSERT INTO users (userName, userLastname, email, userPassword, phone) VALUES ('Raymond', 'Rowlland', 'rayro@mail.com', 'p@ssword', '8091234567');
+
+
+INSERT INTO paymentMethod(cardNumber, expiration, cvv, billingAddress) VALUES ('2034123456574837', '1256', '112', 'C/ Renovacion No.60, Villa Concepcion');
